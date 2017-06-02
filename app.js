083ee629a7a75ea1b75762fab1194462500bfec2
Qaisar Khan <qaisarkhan@Qaisars-iMac.local>
2017-06-02 01:07:37 -0700

    updated HTML. JAVASCRIPT AND CSS

---
+++ app.js
@@ -0,0 +1,29 @@
+//***********************************************
+// DOCUMENT READY
+//***********************************************
+
+$(document).ready(function() {
+  $('.js-like').on('click', function(event) {
+    event.preventDefault();
+    $(this).text('Liked!')
+    .closest('.news-item')
+    .addClass('is-liked');
+  });
+
+  $('.js-add-link').on('click', function(event) {
+      event.preventDefault();
+
+      $('.js-form').toggleClass('is-visible');
+  });
+
+  $('.js-show-modal').on('click', function(event) {
+      event.preventDefault();
+
+      $('.js-modal').addClass('is-visible');
+      $('.js-modal-overlay').addClass('is-visible');
+  });
+  $('.js-modal-overlay').on('click', function(event) {
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
+});
